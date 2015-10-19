d029069f2275d1533a5dd4a266835ecea339b97e
acsent80 <acsent@list.ru>
2015-10-19 13:49:22 +0300

    1

---
+++ src/main/java/model/pojo/Goods.java
@@ -0,0 +1,12 @@
+package model.pojo;
+
+public class Goods {
+
+    private int goodId;
+    private String name;
+
+    public Goods(int goodId, String name) {
+        this.goodId = goodId;
+        this.name = name;
+    }
+}
